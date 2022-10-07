419641951e1a621146ccac17a38d463e6a5b1027
rogseo <rogseo@gmail.com>
2022-10-06 21:40:01 -0500

    Readme changed

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var today=moment().format("dddd, MMMM Do");
-$("#currentDay").text(today); //// ???? when text exist, text didn't work
+$("#currentDay").text(today); 
 var hour=moment().format("H");
 console.log(hour);
 
@@ -27,12 +27,12 @@ var schedules=[
     {id:"3PM", text:""},
     {id:"4PM", text:""},
     {id:"5PM", text:""}];
-    ///스케쥴을 같은걸 여러번 누르면 같이 타임블럭이 여러개 생긴다.
+   
    
 
 $(".saveBtn").on("click", function(e){
     var text=$(e.target).prev().val();
-    // console.log($(e.target).prev().dataset.hour);
+
 
     var id=$(e.target).prev().attr("id");
     console.log(id);
@@ -41,11 +41,6 @@ $(".saveBtn").on("click", function(e){
     obj.text=$(e.target).prev().val();
     
 
-    // schedules[],
-
-    // var hourSchedule={ID:id,text:text};
-    // schedules.push(hourSchedule);
-    // console.log(schedules);
     saveLocalStorage(schedules);
     renderLocalStorage();
         
@@ -73,7 +68,6 @@ function renderLocalStorage()
 function init(){
     var storedSchedules = JSON.parse(localStorage.getItem("hourlySchedule"));
 
-    // If todos were retrieved from localStorage, update the todos array to it
     if (storedSchedules !== null) {
         schedules = storedSchedules;
     }
